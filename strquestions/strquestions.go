f25adf839a08036d14ab8d1b563c85e50af8a826
Alexei Pancratov <drivergear@gmail.com>
2021-06-17 16:45:50 -0400

    Added non-optimized version and benchmarks

--- strquestions/strquestions.go
+++ strquestions/strquestions.go
@@ -1,18 +1,35 @@
 package strquestions
 
 func GetFirstNonRepeatingChar(str string) rune {
-	occurrences := [26]int{}
+	occurrenceCounts := [26]int{}
 
 	for i := 0; i < len(str); i++ {
 		currentChar := rune(str[i])
-		occurrences[currentChar-'a']++
+		occurrenceCounts[currentChar-'a']++
 	}
 
-	for i, o := range occurrences {
-		if o == 1 {
+	for i, oc := range occurrenceCounts {
+		if oc == 1 {
 			return rune(i + 'a')
 		}
 	}
 
+	return '-'
+}
+
+func GetFirstNonRepeatingCharNotOptimized(str string) rune {
+	for i := 0; i < len(str); i++ {
+		var repeatingChar rune = '-'
+
+		for j := 0; j < len(str); j++ {
+			if i != j && str[i] == str[j] {
+				repeatingChar = rune(str[i])
+			}
+		}
+
+		if repeatingChar == '-' {
+			return rune(str[i])
+		}
+	}
 	return '-'
 }
--- strquestions/strquestions_test.go
+++ strquestions/strquestions_test.go
@@ -23,3 +23,38 @@ func TestGetFirstNonRepeatingChar(t *testing.T) {
 		}
 	}
 }
+
+func BenchmarkGetFirstNonRepeatingChar(b *testing.B) {
+	input := "aaaabbbbccccdde"
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		strquestions.GetFirstNonRepeatingChar(input)
+	}
+}
+
+func TestGetFirstNonRepeatingCharNotOptimized(t *testing.T) {
+	scenarios := []struct {
+		input    string
+		expected rune
+	}{
+		{input: "aaabc", expected: 'b'},
+		{input: "aaabcccbda", expected: 'd'},
+		{input: "aaaabbbbccccdde", expected: 'e'},
+		{input: "aaabbbccc", expected: '-'},
+	}
+
+	for _, s := range scenarios {
+		got := strquestions.GetFirstNonRepeatingCharNotOptimized(s.input)
+		if s.expected != got {
+			t.Errorf("Did not meet the expected result. Expected '%q', got '%q'", s.expected, got)
+		}
+	}
+}
+
+func BenchmarkGetFirstNonRepeatingCharNotOptimized(b *testing.B) {
+	input := "aaaabbbbccccdde"
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		strquestions.GetFirstNonRepeatingCharNotOptimized(input)
+	}
+}
