8c1af2ab095f35dc3d8ab692abb63addb37273b2
Alexei Pancratov <drivergear@gmail.com>
2021-07-08 22:20:22 -0400

    Fixed main

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"interviewq/dynamicprogramming"
+	"interviewq/dynamicprogramming/memoization"
 )
 
 func main() {
-	fmt.Printf("Result = %v\n", dynamicprogramming.Fib(6, nil))  // 8
-	fmt.Printf("Result = %v\n", dynamicprogramming.Fib(7, nil))  // 13
-	fmt.Printf("Result = %v\n", dynamicprogramming.Fib(8, nil))  // 21
-	fmt.Printf("Result = %v\n", dynamicprogramming.Fib(50, nil)) // 12586269025
+	fmt.Printf("Result = %v\n", memoization.Fib(6, nil))  // 8
+	fmt.Printf("Result = %v\n", memoization.Fib(7, nil))  // 13
+	fmt.Printf("Result = %v\n", memoization.Fib(8, nil))  // 21
+	fmt.Printf("Result = %v\n", memoization.Fib(50, nil)) // 12586269025
 }
